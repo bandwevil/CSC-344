36fae4b88d571e73ba0c0ac136b44da5295c572f
Tyler Saadus <tsaadus@calpoly.edu>
2014-03-20 17:07:26 -0700

    Cleaned up and commented

--- Final project/myGen.py
+++ Final project/myGen.py
@@ -14,21 +14,16 @@ except NameError:
 
 midi_c3 = 48
 note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-current_root = midi_c3
 
 #Scales are defined by half steps from the root note
-scale_major =       [0, 2, 4, 5, 7, 9, 11, 12, 14, 16]
-scale_harm_minor =  [0, 2, 3, 5, 7, 8, 11, 12, 14, 15]
-scale_min_pent =    [0, 3, 5, 7, 10, 12, 15, 17, 19, 22]
-scale_maj_pent =    [0, 2, 4, 7, 9, 12, 14, 16, 19, 21]
-current_scale = scale_major
+scales =    [[[0, 2, 4, 5, 7, 9, 11, 12, 14, 16], "Major"],
+             [[0, 2, 3, 5, 7, 8, 11, 12, 14, 15], "Harmonic Minor"],
+             [[0, 3, 5, 7, 10, 12, 15, 17, 19, 22], "Minor Pentatonic"],
+             [[0, 2, 4, 7, 9, 12, 14, 16, 19, 21], "Major Pentatonic"]]
 
-play_probability = 0.2
-
-diagonals = False
-born_rule = (1, 3)
-stay_rule = (0, 2, 4)
-    
+#2D List of rules with a tuple for born and stay alive rules, whether to include diagonals, and their names
+rules_list = [[(3,),(2, 3), True, "Conway"], [(1, 3), (0, 2, 4), False, "Parity"], [(2,), (), True, "Seeds"]]
+ 
 class Button:
     def __init__(self, image_off_up, image_off_down, image_on_up, image_on_down, pos_x, pos_y):
         self.image = image_off_up
@@ -40,7 +35,8 @@ class Button:
         self.down = False
         self.alive = False
         self.playing = False
-
+    
+    #Handle image updating if the mouse clicks this cell
     def check_mouse(self, mouse_pos):
         if (pygame.mouse.get_pressed()[0] == 1 and self.pos.collidepoint(mouse_pos)):
             if (self.down == False):
@@ -62,16 +58,18 @@ class Button:
                 self.image = self.on_up
             else:
                 self.image = self.off_up
+    
+    #Randomly set cell to alive or dead
     def randomize(self):
         if (random.getrandbits(1) == 1):
             self.image = self.off_up
         else:
             self.image = self.on_up
         
-
+#Count the number of neighbors a cell has
 def count_neighbors(grid, x, y):
     sum = 0
-    #Super-hackish way to add wrap-around -- Must change if altering size of CA --
+    #hackish way to add wrap-around -- Must change if altering size of CA --
     left = (x-1 if x-1 >= 0 else 9)
     down = (y-1 if y-1 >= 0 else 9)
     right = (x+1 if x+1 <= 9 else 0)
@@ -97,6 +95,7 @@ def count_neighbors(grid, x, y):
         sum += 1
     return sum
         
+#Given a cell, count its neighbors and return the next state of the cell
 def update_cells(current_state, x, y):
     neighbors = count_neighbors(current_state, x, y)
     
@@ -106,15 +105,19 @@ def update_cells(current_state, x, y):
         return True
     else:
         return False
-    
+
+#Triggers MIDI notes based on whether the given cell is alive as well as the current play_probability value
 def cell_to_midi(button, x, y, midi):
-    if button.alive and not button.playing and random.random() < play_probability:
-        midi.note_on(current_root + current_scale[y], 110, ((x + y) % 5))
+    if button.alive and not button.playing:
+        if random.random() < play_probability:
+            midi.note_on(current_root + current_scale[y], 110, ((x + y) % 5))
+        #Set playing even if we're not playing so that we can see that we've decided to not play this cell
         button.playing = True
     elif not button.alive and button.playing:
         midi.note_off(current_root + current_scale[y], None, ((x + y) % 5))
         button.playing = False
-        
+
+#Takes alive state from a 2d list of Buttons and saves it to a 2D list of bools
 def save_to_list(cells, list):
     i = 0
     for o in cells:
@@ -123,7 +126,8 @@ def save_to_list(cells, list):
             list[i][j] = l.alive
             j += 1
         i += 1
-        
+
+#Takes bools from a 2D list and sets a 2D list of Buttons to the values contained
 def load_from_list(cells, list):
     i = 0
     for o in cells:
@@ -133,14 +137,17 @@ def load_from_list(cells, list):
             j += 1
         i += 1
 
+#Get the number of milliseconds between beats at a given beats per minute value
 def bpm_to_ms(bpm):
     return 60000/bpm
     
+#Stop every possible MIDI note for the 5 channels we use
 def stop_all_midi(midi_out):
     for i in range(127):
         for j in range(5):
             midi_out.note_off(i, None, j)
             
+#Copies alive state from one 2D list of Buttons to another
 def copy_obj_state(obj_from, obj_to):
     i = 0
     for o in obj_from:
@@ -149,7 +156,6 @@ def copy_obj_state(obj_from, obj_to):
             obj_to[i][j].alive = l.alive
             j += 1
         i += 1
-    
 
 def main_gen(device_id):
     pygame.init()
@@ -187,17 +193,15 @@ def main_gen(device_id):
     #Draw loading arrows to screen (un-animated, so we only have to blit them this once)
     screen.blit(arrow_down, (115, 285))
     screen.blit(arrow_up, (150, 285))
-    
+    #Collision rectangles for checking if the arrows are clicked
     down_mesh = pygame.Rect(115, 285, 28, 42)
     up_mesh = pygame.Rect(150, 285, 28, 42)
     
     play_pause = paused_icon
     playing = False
     
-    loop_value = 16
-    loop_curr_val = 0
-    looping = False
-    
+    #Globals for modifying play parameters
+    #Could probably be moved to its own class, but this works without much grossness
     global diagonals
     global born_rule
     global stay_rule
@@ -205,8 +209,25 @@ def main_gen(device_id):
     global current_root
     global play_probability
     
-    rule_text = font.render("Rule: Parity", True, (240, 240, 240))
-    scale_text = font.render("Major", True, (240, 240, 240))
+    #Set up default values
+    loop_value = 16
+    loop_curr_val = 0
+    looping = False
+    
+    scale_num = 0
+    current_scale = scales[scale_num][0]
+    current_root = midi_c3
+    
+    rule_num = 0
+    born_rule = rules_list[rule_num][0]
+    stay_rule = rules_list[rule_num][1]
+    diagonals = rules_list[rule_num][2]
+    
+    play_probability = 0.2
+    
+    #Initialize text printouts
+    rule_text = font.render("Rule: " + rules_list[rule_num][3], True, (240, 240, 240))
+    scale_text = font.render(scales[scale_num][1], True, (240, 240, 240))
     scale_label = font.render("Scale:", True, (240, 240, 240))
     key_text = font.render("Key: " + note_names[current_root - midi_c3], True, (240, 240, 240))
     tempo_text = font.render("Tempo: " + str(current_bpm), True, (240, 240, 240))
@@ -235,6 +256,7 @@ def main_gen(device_id):
     # Main loop
     while 1:
         for event in pygame.event.get():
+            #Check mouse click on up/down arrows
             if event.type == MOUSEBUTTONDOWN and event.button == 1:
                 if down_mesh.collidepoint(event.pos):
                     copy_obj_state(objects_input, objects_sim)
@@ -242,9 +264,11 @@ def main_gen(device_id):
                 elif up_mesh.collidepoint(event.pos):
                     copy_obj_state(objects_sim, objects_input)
             elif event.type == KEYDOWN:
+                #Quit pygame
                 if event.key == K_ESCAPE:
                     stop_all_midi(midi_out)
                     sys.exit()
+                #Toggle between paused and playing
                 elif event.key == K_SPACE:
                     playing = not playing
                     
@@ -254,48 +278,46 @@ def main_gen(device_id):
                     else:
                         play_pause = playing_icon
                         
-                #Clear current cells
+                #Clear sim (lower) cells
                 elif event.key == K_v:
                     stop_all_midi(midi_out)
                     for l in objects_sim:
                         for o in l:
                             o.alive = False
-                            
+                
+                #Clear input (upper) cells
                 elif event.key == K_c:
                     for l in objects_input:
                         for o in l:
                             o.alive = False
                             
-                #Switch to Conway ruleset
+                #Switch ruleset down
                 elif event.key == K_q:
-                    rule_text = font.render("Rule: Conway", True, (240, 240, 240))
-                    diagonals = True
-                    born_rule = (3,)
-                    stay_rule = (2, 3)
+                    rule_num -= 1
+                    if rule_num < 0:
+                        rule_num = len(rules_list) - 1
+                    born_rule = rules_list[rule_num][0]
+                    stay_rule = rules_list[rule_num][1]
+                    diagonals = rules_list[rule_num][2]
+                    rule_text = font.render("Rule: " + rules_list[rule_num][3], True, (240, 240, 240))
                     
-                #Switch to Parity ruleset
+                #Switch ruleset up
                 elif event.key == K_w:
-                    rule_text = font.render("Rule: Parity", True, (240, 240, 240))
-                    diagonals = False
-                    born_rule = (1, 3)
-                    stay_rule = (0, 2, 4)
+                    rule_num += 1
+                    if rule_num >= len(rules_list):
+                        rule_num = 0
+                    born_rule = rules_list[rule_num][0]
+                    stay_rule = rules_list[rule_num][1]
+                    diagonals = rules_list[rule_num][2]
+                    rule_text = font.render("Rule: " + rules_list[rule_num][3], True, (240, 240, 240))
                     
                 #Switch between scales
                 elif event.key == K_a:
                     stop_all_midi(midi_out)
-                    
-                    if current_scale == scale_major:
-                        current_scale = scale_harm_minor
-                        scale_text = font.render("Harmonic Minor", True, (240, 240, 240))
-                    elif current_scale == scale_harm_minor:
-                        current_scale = scale_min_pent
-                        scale_text = font.render("Minor Pentatonic", True, (240, 240, 240))
-                    elif current_scale == scale_min_pent:
-                        current_scale = scale_maj_pent
-                        scale_text = font.render("Major Pentatonic", True, (240, 240, 240))
-                    elif current_scale == scale_maj_pent:
-                        current_scale = scale_major
-                        scale_text = font.render("Major", True, (240, 240, 240))
+                    scale_num += 1
+                    if scale_num >= len(scales):
+                        scale_num = 0
+                    scale_text = font.render(scales[scale_num][1], True, (240, 240, 240))
                 
                 #Cycle through keys downwards
                 elif event.key == K_s:
@@ -352,16 +374,17 @@ def main_gen(device_id):
                         loop_label = font.render("Looping", True, (240, 240, 240))
                     else:
                         loop_label = font.render("Not looping", True, (240, 240, 240))
-                        
+                #Decrease loop size
                 elif event.key == K_o:
                     if loop_value > 2:
                         loop_value -= 1
                         loop_text = font.render("Count: " + str(loop_value), True, (240, 240, 240))
+                #Increase loop size
                 elif event.key == K_p:
                     loop_value += 1
                     loop_text = font.render("Count: " + str(loop_value), True, (240, 240, 240))
                         
-        
+        #State update check
         if playing and time.time() * 1000 - last_state_time > state_update_time:
             last_state_time = time.time() * 1000
             
@@ -383,12 +406,14 @@ def main_gen(device_id):
                     j += 1
                 i += 1
             
+            #If we're looping, check to see if we need to reset
             if looping:
                 loop_curr_val += 1
                 if loop_curr_val >= loop_value:
                     loop_curr_val = 0
                     load_from_list(objects_sim, loop_init)
-            
+        
+        #Frame update check
         if time.time() * 1000 - last_frame_time > frame_update_time:
             last_frame_time = time.time() * 1000
             # Clear current display stuff
@@ -401,6 +426,7 @@ def main_gen(device_id):
                 for o in l:
                     o.check_mouse(pygame.mouse.get_pos())
                     screen.blit(background, o.pos, o.pos)
+            
             # Redraw updated image
             screen.blit(play_pause, (300, 10))
             screen.blit(rule_text, (300, 70))
@@ -418,7 +444,8 @@ def main_gen(device_id):
                 for o in l:
                     screen.blit(o.image, o.pos)
             pygame.display.update()
-    
+
+#Scan current MIDI devices, print out their information
 def print_device_info():
     pygame.midi.init()
     for i in range( pygame.midi.get_count() ):
@@ -434,7 +461,8 @@ def print_device_info():
         print ("%2i: interface :%s:, name :%s:, opened :%s:  %s" %
                (i, interf, name, opened, in_out))
     pygame.midi.quit()
-    
+
+#Print out possible command line flags
 def print_usage():
     print("Usage:")
     print("-o [device_id] - main program, define MIDI output device")
